60437037bcf5c614c596ecc3c7cd54ac338d1784
user1 <shadow1212@21cn.com>
2018-02-05 18:09:37 +0800

    update the start up script

--- tests/test.pl
+++ tests/test.pl
@@ -10,6 +10,10 @@ if ($ARGV[$#ARGV]==0 || $ARGV[$#ARGV]==1) {
   #system "rm -f core";
   system "killall myproxy_app" ;
   if ($ARGV[$#ARGV]==1) {
+     system "sudo echo \"20000\" > /proc/sys/net/core/somaxconn" ;
+     system "sudo echo \"20000\" > /proc/sys/net/ipv4/tcp_max_syn_backlog" ;
+     system "ulimit -n 20000" ;
+
      system "./src/myproxy_app -c ./conf/configure_pressure_test.json &  " ;
 #    system "./src/myproxy_app -c ./conf/configure.json &  " ;
   }
